79601797d9d4f08c2bc3cda81efd4c2d9497a1a1
Rasia83 <tiago_rasia@hotmail.com>
2023-07-09 11:34:14 -0300

    permitir que o metodo criar retorne o ID do novo cadastro

--- src/main/java/org/example/repository/ProfessorRepository.java
+++ src/main/java/org/example/repository/ProfessorRepository.java
@@ -5,7 +5,7 @@ import org.example.model.Professor;
 import java.util.List;
 
 public interface ProfessorRepository {
-    void criarProfessor(Professor professor);
+    int criarProfessor(Professor professor);
     List<Professor> listarProfessores();
     Professor deletarProfessor(int id);
     void atualizarProfessor(Professor professor);
